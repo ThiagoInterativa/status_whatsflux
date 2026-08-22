26fb961006624dbc2792308027cf7338e4d87f89
Thiago Lima <lima@interativanet.com.br>
2026-08-22 18:47:29 -0300

    Update statusWhatsFkux.py

--- statusWhatsFkux.py
+++ statusWhatsFkux.py
@@ -2745,7 +2745,7 @@ with col_kanban:
                     col_edit,
                     col_del
                 ) = st.columns(
-                    [0.95, 0.08, 0.08],
+                    [1.95, 0.08, 0.08],
                     vertical_alignment="center"
                 )
 
