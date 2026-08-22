2abb967c3b8841874b36fa13b691b5e132845e6f
Thiago Lima <lima@interativanet.com.br>
2026-08-22 18:54:31 -0300

    Update statusWhatsFkux.py

--- statusWhatsFkux.py
+++ statusWhatsFkux.py
@@ -2931,7 +2931,7 @@ with col_rm:
         if pendencias_rm:
 
             col_qtd, col_del = st.columns(
-                [0.95, 0.10],
+                [0.95, 0.05],
                 vertical_alignment="center"
             )
 
